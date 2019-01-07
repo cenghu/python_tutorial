858e364a1d83e53bd3d9a168342d651faa467c17
Chen Huang <ceng.huang@gmail.com>
2019-01-07 13:17:50 +0100

    second commit

--- data_analysis_4D/example.py
+++ data_analysis_4D/example.py
@@ -4,7 +4,7 @@ Created on Tue Jul  3 10:01:12 2018
 
 @author: ChenHu
 """
-
+# import some packages for computing and plotting
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -14,13 +14,15 @@ font = {'size'   : 20}
 plt.rc('font', **font)
 plt.rc('figure', figsize=(6.4, 4.8))
 
+#import csv datafile
 database_4D=pd.read_csv('database_4D.csv', sep=';', skiprows=0, header=0,encoding='windows-1252')
 
 database_4D=database_4D.sort_values(by=['Pmax'])
 
-Pmax=database_4D.iloc[:,3]
-KSt=database_4D.iloc[:,4]
-# use polyfit for fitting Pmax and KSt by least squared errors
+Pmax=database_4D.iloc[:,3] # put column index number 3 into Pmax
+KSt=database_4D.iloc[:,4] # put column index number 4 into KSt
+
+# use polyfit for fitting Pmax and KSt using 2nd order polynomial by least squared errors
 fit, residual, _, _, _ = np.polyfit(Pmax,KSt,2,full=True)
 
 # fit_fn the fit function
@@ -33,26 +35,25 @@ rel_error_max=max(abs(KSt_approx-KSt)/KSt)
 rel_error_mean=np.mean(abs(KSt_approx-KSt)/KSt)
 
 
-cm = plt.cm.get_cmap('RdYlBu')
+cm = plt.cm.get_cmap('RdYlBu') #get red yellow blue color map
 #plot    
 plt.clf()
 
-#fill_colors = ['#4169e1' if MC<=5. else '#000000' for MC in list(databasePmaxKst_yesD50_yesMc['MC'])]
+#scattered plot
 sc=plt.scatter(Pmax,KSt, s=database_4D.iloc[:,1]*2.5, 
                c=database_4D.iloc[:,2], alpha=0.4, edgecolors='k', cmap=cm)
 
-plt.plot(Pmax,fit_fn(Pmax), color='k',linestyle='-',lw=2)
+plt.plot(Pmax,fit_fn(Pmax), color='k',linestyle='-',lw=2) # fit plot
 #plot the data from the current experiment
 
 plt.colorbar(sc)
 plt.xlabel('$P_\mathrm{max}$ [bar]')
 plt.ylabel('$K_{St}$ [bar$\cdot$m/s]')
-#lgd=plt.legend(bbox_to_anchor=(1, 1), loc=2, borderaxespad=0., fontsize='small')
 
 plt.annotate(r'moisture content [%]',
              xy=(0.7, 1.03), xycoords='axes fraction',
               fontsize=15)
-#plt.axis([0, 10, 0, 200])
+
 plt.grid(True,linestyle='dashed')
 # fix the tickers
 ax = plt.gca()
